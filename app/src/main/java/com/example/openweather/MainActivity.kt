51b759b76ca2b2c7de95cf8387dab971f30d7edf
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2023-07-28 06:48:30 +0700

    Implement Datastore to save weather data locally

--- app/src/main/java/com/example/openweather/MainActivity.kt
+++ app/src/main/java/com/example/openweather/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.openweather
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.preferencesDataStore
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel=MainViewModel()
+    companion object{
+        private val Context.dataStore by preferencesDataStore(name = "settings")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel=MainViewModel(applicationContext.dataStore)
         mainViewModel.getCurrentLocation(this)
     }
 }
--- app/src/main/java/com/example/openweather/MainViewModel.kt
+++ app/src/main/java/com/example/openweather/MainViewModel.kt
@@ -9,24 +9,47 @@ import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Build
 import android.provider.Settings
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
 import androidx.lifecycle.*
 import com.bumptech.glide.Glide
+import com.example.openweather.MainViewModel.PreferencesKeys.DESCRIPTION
+import com.example.openweather.MainViewModel.PreferencesKeys.ICON
+import com.example.openweather.MainViewModel.PreferencesKeys.NAME
+import com.example.openweather.MainViewModel.PreferencesKeys.TEMPERATURE
+import com.example.openweather.MainViewModel.PreferencesKeys.TIME
 import com.example.openweather.model.WeatherData
 import com.example.openweather.network.ApiClient
 import com.example.openweather.network.OpenWeatherMapService
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Tasks.await
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
 import java.util.*
 
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val dataStore: DataStore<Preferences>) : ViewModel() {
+    private object PreferencesKeys {
+        val TEMPERATURE = doublePreferencesKey("temperature")
+        val NAME = stringPreferencesKey("name")
+        val DESCRIPTION = stringPreferencesKey("description")
+        val ICON = stringPreferencesKey("icon")
+        val TIME = stringPreferencesKey("time")
+    }
 
     private val openWeatherMapService: OpenWeatherMapService =
         ApiClient.instance!!.create(OpenWeatherMapService::class.java)
@@ -52,7 +75,6 @@ class MainViewModel : ViewModel() {
                 }
             }
             requestLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            println("why")
         } else {
             getWeather(mainActivity)
         }
@@ -60,19 +82,59 @@ class MainViewModel : ViewModel() {
 
     @SuppressLint("MissingPermission")
     fun getWeather(mainActivity: MainActivity) = viewModelScope.launch {
+        val nameTextView = mainActivity.findViewById<TextView>(R.id.location)
+        val timeTextView = mainActivity.findViewById<TextView>(R.id.time)
+        val iconImageView = mainActivity.findViewById<ImageView>(R.id.weather_icon)
+        val descriptionTextView = mainActivity.findViewById<TextView>(R.id.description)
+        val temperatureTextView = mainActivity.findViewById<TextView>(R.id.temperature)
+
+        val temperatureFlow : Double = dataStore.data.map {
+            it[TEMPERATURE] ?: 0.0
+        }.first()
+        var tempText = "${temperatureFlow.toInt()- 273}\u00B0C"
+        temperatureTextView.text=tempText
+        val nameFlow : String = dataStore.data.map {
+            it[NAME] ?: "Location"
+        }.first()
+        val timeFlow : String = dataStore.data.map {
+            it[TIME] ?: "Last Updated 12:00"
+        }.first()
+        temperatureTextView.text=tempText
+        nameTextView.text=nameFlow
+        timeTextView.text=timeFlow
+        val descriptionFlow : String = dataStore.data.map {
+            it[DESCRIPTION] ?: "Unknown weather"
+        }.first()
+        descriptionTextView.text=descriptionFlow
+        val iconFlow : String = dataStore.data.map {
+            it[ICON] ?: "50d"
+        }.first()
+        Glide.with(mainActivity)
+            .load("http://openweathermap.org/img/w/$iconFlow.png")
+            .into(mainActivity.findViewById(R.id.weather_icon))
+
+//        val weatherData: Flow<WeatherPreferences>=dataStore.data
+//            .map { preference ->
+//                val temperature = preference[TEMPERATURE] ?: 20.0
+//                val name = preference[NAME] ?: "Location"
+//                val description = preference[DESCRIPTION] ?: "Weather Description"
+//                val icon = preference[ICON] ?: "10d"
+//                val time = preference[TIME] ?: "12:00"
+//
+//                println(icon)
+//                nameTextView.text=name
+//                descriptionTextView.text=description
+//                timeTextView.text=time
+//                temperatureTextView.text=temperature.toString()
+//            }
+
         val lm = mainActivity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         var gps_enabled = false
-        var network_enabled = false
 
         try {
             gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
         } catch (ex: Exception) {
         }
-
-//        try {
-//            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-//        } catch (ex: Exception) {
-//        }
 
         if (!gps_enabled) {
             // notify user
@@ -88,30 +150,61 @@ class MainViewModel : ViewModel() {
                 }
                 .setNegativeButton("Cancel", null)
                 .show()
-        }
-        else{
+        } else{
+            if(!isOnline()){
+                Toast.makeText(mainActivity,"Not connected to internet",Toast.LENGTH_SHORT).show()
+                return@launch
+            }
             val locationServices = LocationServices.getFusedLocationProviderClient(mainActivity)
             val result = locationServices.lastLocation.await()
-            println("${result.latitude} : ${result.longitude}")
+            try{
+                println(result.latitude)
+            } catch (e: java.lang.NullPointerException){
+                /**
+                Handle an error when the application treated by exactly this steps:
+                 1. turn off internet,location,also revoke location permission
+                 2. then give permission to location
+                 3. turn on internet
+                 4. sometimes val result will null, not consistent
+                */
+                Toast.makeText(mainActivity,"Failed to get current location",Toast.LENGTH_SHORT).show()
+                Log.e("error","Failed to get current location")
+                return@launch
+            }
             openWeatherMapService.getCurrentWeatherDataByLatLon(
                 result.latitude.toString(),
                 result.longitude.toString(),
                 API_KEY
             )?.enqueue(object :
                 Callback<WeatherData?> {
-                override fun onResponse(call: Call<WeatherData?>, response: Response<WeatherData?>) {
+                override fun onResponse(
+                    call: Call<WeatherData?>,
+                    response: Response<WeatherData?>
+                ) {
                     if (response.isSuccessful) {
                         val weatherData: WeatherData = response.body()!!
-                        mainActivity.findViewById<TextView>(R.id.location).text = weatherData.name
-                        val temperatureText="${weatherData.main.temp.toInt()-273}\u00B0C"
-                        mainActivity.findViewById<TextView>(R.id.temperature).text = temperatureText
-                        mainActivity.findViewById<TextView>(R.id.description).text =
+                        nameTextView.text = weatherData.name
+                        val temperatureText = "${weatherData.main.temp.toInt() - 273}\u00B0C"
+                        temperatureTextView.text = temperatureText
+                        descriptionTextView.text =
                             weatherData.weather[0].description
-                        Glide.with(mainActivity).load("http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png")
-                            .into(mainActivity.findViewById(R.id.weather_icon))
+                        Glide.with(mainActivity)
+                            .load("http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png")
+                            .into(iconImageView)
                         val calendar: Calendar = Calendar.getInstance(Locale.getDefault())
-                        val timeText="Last Updated ${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}"
-                        mainActivity.findViewById<TextView>(R.id.time).text = timeText
+                        val timeText = "Last Updated ${calendar.get(Calendar.HOUR_OF_DAY)}:"+String.format("%02d",calendar.get(Calendar.MINUTE))
+                        timeTextView.text = timeText
+
+                        viewModelScope.launch {
+                            dataStore.edit { preference ->
+                                preference[TEMPERATURE] = weatherData.main.temp
+                                preference[NAME] = weatherData.name
+                                preference[DESCRIPTION] = weatherData.weather[0].description
+                                preference[ICON] = weatherData.weather[0].icon
+                                preference[TIME] = timeText
+                                println(preference[TIME])
+                            }
+                        }
                     }
                 }
 
@@ -120,5 +213,19 @@ class MainViewModel : ViewModel() {
                 }
             })
         }
+    }
+
+    fun isOnline(): Boolean {
+        val runtime = Runtime.getRuntime()
+        try {
+            val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+            val exitValue = ipProcess.waitFor()
+            return exitValue == 0
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        return false
     }
 }
